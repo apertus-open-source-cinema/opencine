4b956daa38ed511d673d9853e917062a8d3f655c
BAndiT1983 <b.andrej@cumir.de>
2015-07-21 18:34:01 +0200

    - Made adjustments to enable successful compiling - Added platform-dependent code to CMake

--- Source/OCBackup/OCBackup.cpp
+++ Source/OCBackup/OCBackup.cpp
@@ -2,41 +2,30 @@
 #include <memory>
 
 #include <OCui.h>
+#include <OCCore.h>
 
 #include "OCBackup.h"
+
+#include "Presenters/BackupPresenter.h"
 #include "Layouts/BackupLayout.h"
-
-#include <xxhash.h>
-
-/*void TestFunc()
-{
-    int i = 0;
-}*/
 
 class OCBackup : public OCui::GUIApplication
 {
     std::shared_ptr<QWidget> _layout;
+    std::shared_ptr<IBackupPresenter> _presenter;
 
 public:
     OCBackup(int argc, char** argv) : GUIApplication(argc, argv, "OCBackup")
     {
-        _layout = std::make_shared<OCBackupLayout>();
+        _presenter = std::make_shared<BackupPresenter>();
+        _layout = std::make_shared<OCBackupLayout>(nullptr, _presenter.get());
         _layout->showFullScreen();
         SetLayout(*_layout.get());
-
-        std::string test = "1234";
-        unsigned __int32 hash = XXH32(test.c_str(), 4, 1);
-        std::wstring test2 = L"1234";
-        unsigned __int64 hash2 = XXH64(test2.c_str(), 4, 1);
-
-        int i = 0;
     }
 };
 
 int main(int argc, char** argv)
 {
-    std::cout << "OCBackup" << std::endl;
-
     std::shared_ptr<OCBackup> backup = std::make_shared<OCBackup>(argc, argv);
     return backup->Run();
 }
