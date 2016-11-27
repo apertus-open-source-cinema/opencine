5420726efd0f6dc4a40da7c19324c892dee70edd
BAndiT1983 <b.andrej@cumir.de>
2016-11-27 22:44:23 +0100

    - T343: Adjusted progress lsit delegate, removed some obsolete code

--- Source/OCui/Controls/TaskProgressDelegate.cpp
+++ Source/OCui/Controls/TaskProgressDelegate.cpp
@@ -3,9 +3,15 @@
 #include <QApplication>
 #include <QPainter>
 #include <QProgressBar>
+#include <QHBoxLayout>
+#include <QLabel>
 
-TaskProgressDelegate::TaskProgressDelegate(QWidget* parent) : QStyledItemDelegate(parent)
+TaskProgressDelegate::TaskProgressDelegate(QWidget* parent) : QStyledItemDelegate(parent),
+                                                              font(QApplication::font()),
+                                                              subFont(QApplication::font())
 {
+	font.setBold(true);
+	subFont.setPointSize(font.pointSize() - 2);
 }
 
 TaskProgressDelegate::~TaskProgressDelegate()
@@ -14,107 +20,64 @@ TaskProgressDelegate::~TaskProgressDelegate()
 
 QSize TaskProgressDelegate::sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const
 {
-	//QIcon icon = qvariant_cast<QIcon>(index.data(IconRole));
-	//QSize iconsize = icon.actualSize(option.decorationSize);
-	QFont font = QApplication::font();
-	QFontMetrics fm(font);
-
-	return QSize(200, 70);// (QSize(iconsize.width(), iconsize.height() + fm.height() + 8));
+	return QSize(option.rect.width(), 70);
 }
 
 void TaskProgressDelegate::paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const
 {
+	// Draws default things, like focus rectangle
 	QStyledItemDelegate::paint(painter, option, index);
 
 	painter->save();
 
-	QFont font = QApplication::font();
-	QFont subFont = QApplication::font();
-	//font.setPixelSize(font.weight()+);
-	font.setBold(true);
-	subFont.setWeight(subFont.weight() - 5);
-	subFont.setPointSize(8);
-	font.setCapitalization(QFont::AllUppercase);
 	QFontMetrics fm(font);
 
-	QString headerText = "";
-	QString subText = "";
-	//QIcon icon = qvariant_cast<QIcon>(index.data(IconRole));
-	QVariant variant = index.model()->data(index);
-	//QStandardItemModel *sModel = qobject_cast<QStandardItemModel *>(model);
-	QVariant item = index.data(Qt::DisplayRole);// sModel->itemFromIndex(index.model()->index(0, 0));
-	QStringList test2 = item.toStringList();
-	QStringList test = variant.toStringList();
+	QString headerText = "?";
+	QString subText = "?";
+
 	QStringList values = index.model()->data(index).toStringList();
 	if (!values.empty())
 	{
-		headerText = values.at(0); //_taskDescription; // qvariant_cast<QString>("Test123");
+		headerText = values.at(0);
 		subText = values.at(1);
 	}
-	//QSize iconsize = icon.actualSize(option.decorationSize);
 
 	QRect headerRect = option.rect;
-	headerRect.setBottom(headerRect.bottom() / 2);
-	QRect subheaderRect = option.rect;
-	QRect iconRect = subheaderRect;
-
-	//iconRect.setRight(iconsize.width() + 30);
-	//iconRect.setTop(iconRect.top() + 5);
 	headerRect.setLeft(10);
-	subheaderRect.setLeft(10);
-	subheaderRect.setTop(headerRect.bottom() + 10);
 	headerRect.setTop(headerRect.top() + 10);
-	headerRect.setBottom(headerRect.top() + fm.height());
+	headerRect.setWidth(fm.width(headerText + " | "));
+	headerRect.setBottom(headerRect.top() + fm.height() + 5);
 
-	subheaderRect.setTop(headerRect.bottom() + 2);
-
-	//painter->drawPixmap(QPoint(iconRect.right()/2,iconRect.top()/2),icon.pixmap(iconsize.width(),iconsize.height()));
-	//painter->drawPixmap(QPoint(iconRect.left() + iconsize.width() / 2 + 2, iconRect.top() + iconsize.height() / 2 + 3), icon.pixmap(iconsize.width(), iconsize.height()));
+	QRect subheaderRect = option.rect;
+	subheaderRect.setTop(subheaderRect.top() + fm.height() / 2);
+	subheaderRect.setLeft(headerRect.width() + 10);
 
 	painter->setFont(font);
-	painter->drawText(headerRect, headerText);
-
-
+	painter->drawText(headerRect, headerText + " | ");
 	painter->setFont(subFont);
-	painter->drawText(subheaderRect.left(), subheaderRect.top() + 17, subText);
+	painter->drawText(subheaderRect, subText);
 
-	painter->save();
-	painter->translate(option.rect.width() / 5, option.rect.y() + option.rect.height() / 2);
+	// Automatic layout, just for test atm
+	//QWidget widgetTest;
+	//QHBoxLayout layout;
+	//layout.addWidget(new QLabel("Test1"));
+	//layout.addWidget(new QLabel("Test2"));
+	//widgetTest.setLayout(&layout);
+	//widgetTest.render(painter, option.rect.topLeft());
+
+	painter->translate(10, headerRect.bottom());
+
 
 	// TODO: Replace by a new progress bar, which supports custom animations, e.g. for indeterminate state
-	QProgressBar renderer;
-	renderer.resize(option.rect.size().width() / 5 * 4 - 10, option.rect.size().height());
-	//renderer.setValue(35);
-	renderer.setRange(0, 0);
-	renderer.setValue(0);
-
-	renderer.render(painter);
+	QProgressBar progressBar;
+	progressBar.resize(option.rect.size().width() - 10, option.rect.size().height());
+	progressBar.setValue(35);
+	progressBar.render(painter);
 
 	painter->restore();
 }
 
-QString TaskProgressDelegate::GetTaskDescription() const
+QWidget* TaskProgressDelegate::createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) const
 {
-	return _taskDescription;
-}
-
-void TaskProgressDelegate::SetTaskDescription(QString description)
-{
-	_taskDescription = description;
-}
-
-void TaskProgressDelegate::setModelData(QWidget* editor, QAbstractItemModel* model, const QModelIndex& index) const
-{
-	QString str = index.model()->data(index).toString();
-	_taskDescription = str;
-	//SetTaskDescription("Test");
-}
-
-void TaskProgressDelegate::setEditorData(QWidget* editor, const QModelIndex& index) const
-{
-	//_taskDescription = index.model()->data(index).toString();
-	QString str = index.model()->data(index).toString();
-	_taskDescription = str;
-	//SetTaskDescription(taskDescription);
-	//_taskDescription = taskDescription;
+	return nullptr;
 }
--- Source/OCui/Controls/TaskProgressDelegate.h
+++ Source/OCui/Controls/TaskProgressDelegate.h
@@ -5,9 +5,8 @@ class TaskProgressDelegate : public QStyledItemDelegate
 {
 	Q_OBJECT
 
-	Q_PROPERTY(QString taskDescription READ GetTaskDescription WRITE SetTaskDescription)
-
-	mutable QString _taskDescription;
+	QFont font;
+	QFont subFont ;
 
 public:
 	TaskProgressDelegate(QWidget* parent = Q_NULLPTR);
@@ -16,9 +15,5 @@ public:
 	QSize sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const override;
 
 	void paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
-
-	QString GetTaskDescription() const;
-	void SetTaskDescription(QString description);
-	void setModelData(QWidget* editor, QAbstractItemModel* model, const QModelIndex& index) const override;
-	void setEditorData(QWidget* editor, const QModelIndex& index) const override;
+	QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
 };
