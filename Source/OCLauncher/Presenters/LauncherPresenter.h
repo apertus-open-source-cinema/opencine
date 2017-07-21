4202af9057d60cc30167f08277f83c4f867cf7f4
Unknown <b.andrej@cumir.de>
2017-07-22 01:30:33 +0200

    - Added license headers for OCLauncher

--- Source/OCLauncher/Presenters/LauncherPresenter.h
+++ Source/OCLauncher/Presenters/LauncherPresenter.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2017 apertus° Association & contributors
+// Project: OpenCine / OCLauncher
+// License: GNU GPL Version 3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+
 #ifndef LAUNCHERPRESENTER_H
 #define LAUNCHERPRESENTER_H
 
