c463357b8ba11da6b6e6b42c42951b4e7b126acf
Unknown <b.andrej@cumir.de>
2017-07-22 01:26:49 +0200

    - Added license headers to OCui

--- Source/OCui/Helper/QMLThumbnailProvider.cpp
+++ Source/OCui/Helper/QMLThumbnailProvider.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2017 apertus° Association & contributors
+// Project: OpenCine / OCui
+// License: GNU GPL Version 3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+
 #include "QMLThumbnailProvider.h"
 
 #include <memory>
--- Source/OCui/OCui.cpp
+++ Source/OCui/OCui.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2017 apertus° Association & contributors
-// Project: OpenCine / OCBackup
+// Project: OpenCine / OCui
 // License: GNU GPL Version 3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 
 #include <memory>
