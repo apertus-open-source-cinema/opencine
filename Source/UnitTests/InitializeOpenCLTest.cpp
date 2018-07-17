27619b65182433df6749049bde74f880b8be5f74
TofuLynx <cfpgcp3@gmail.com>
2018-07-17 19:58:36 +0100

    Expanded evaluation in OCL unit test.

--- Source/UnitTests/InitializeOpenCLTest.cpp
+++ Source/UnitTests/InitializeOpenCLTest.cpp
@@ -20,14 +20,14 @@ TEST_CASE("Initialization of OpenCL Test", "[OC::Image]")
     };
 
     uint16_t inputRed[inputDataLength] = {
-        0,0,0,0,0,0,0,0,
-        0,1,0,1,0,1,0,1,
-        0,0,0,0,0,0,0,0,
-        0,1,0,1,0,1,0,1,
-        0,0,0,0,0,0,0,0,
-        0,1,0,1,0,1,0,1,
-        0,0,0,0,0,0,0,0,
-        0,1,0,1,0,1,0,1
+        9,9,9,9,9,9,9,9,
+        9,9,9,9,9,9,9,9,
+        9,9,9,9,9,9,9,9,
+        9,9,9,9,9,9,9,9,
+        9,9,9,9,9,9,9,9,
+        9,9,9,9,9,9,9,9,
+        9,9,9,9,9,9,9,9,
+        9,9,9,9,9,9,9,9
     };
 
     uint16_t inputGreen[inputDataLength] = {
@@ -52,6 +52,17 @@ TEST_CASE("Initialization of OpenCL Test", "[OC::Image]")
         0,0,0,0,0,0,0,0
     };
 
+    uint16_t expectedRed[inputDataLength] = {
+        7,7,7,7,7,7,7,7,
+        7,7,7,7,7,7,7,7,
+        7,7,7,7,7,7,7,7,
+        7,7,7,7,7,7,7,7,
+        7,7,7,7,7,7,7,7,
+        7,7,7,7,7,7,7,7,
+        7,7,7,7,7,7,7,7,
+        7,7,7,7,7,7,7,7
+    };
+
     OCImage* inputImage = new OCImage();
     inputImage->SetWidth(8);
     inputImage->SetHeight(8);
@@ -61,10 +72,6 @@ TEST_CASE("Initialization of OpenCL Test", "[OC::Image]")
     inputImage->SetGreenChannel(inputGreen);
     inputImage->SetBlueChannel(inputBlue);
 
-    uint16_t* imageRed = (uint16_t*)inputImage->RedChannel();
-    uint16_t* imageGreen = (uint16_t*)inputImage->GreenChannel();
-    uint16_t* imageBlue = (uint16_t*)inputImage->BlueChannel();
-
     bool status = 0;
 
     status = initializeHost();
@@ -73,8 +80,34 @@ TEST_CASE("Initialization of OpenCL Test", "[OC::Image]")
 
     status = loadImageOCL(*inputImage);
 
+    //status = runImageFillKernel(7);
+
+    OCImage* outputImage = new OCImage();
+    outputImage->SetWidth(8);
+    outputImage->SetHeight(8);
+    outputImage->SetBayerPattern(BayerPattern::BGGR);
+
+    status = saveImageOCL(*outputImage);
+
     status = cleanupOCL();
 
+    uint16_t* imageRed = (uint16_t*)outputImage->RedChannel();
+    uint16_t* imageGreen = (uint16_t*)outputImage->GreenChannel();
+    uint16_t* imageBlue = (uint16_t*)outputImage->BlueChannel();
+
+    bool correctRed = true;
+
+    for(int index = 0; index < inputDataLength; index++)
+    {
+        if(imageRed[index] != expectedRed[index])
+        {
+            correctRed = false;
+            OC_LOG_INFO("index" + std::to_string(index) + " out:" + std::to_string(imageRed[index])+ " exp:" + std::to_string(expectedRed[index]));
+            break;
+        }
+    }
+
     REQUIRE(status == 0);
+    REQUIRE(correctRed == true);
 }
 
